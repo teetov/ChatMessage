05430b5767a0caeb45cf1dc2eaf40dbb66647908
Aleksey Titov <Titov.A.G@sberbank.ru>
2018-08-10 18:18:18 +0300

    some changes

--- src/main/java/com/teetov/chat/message/MessageFactory.java
+++ src/main/java/com/teetov/chat/message/MessageFactory.java
@@ -1,20 +1,20 @@
 package com.teetov.chat.message;
 
 public class MessageFactory {
-	
-	String name;
-	String senderAddress;
-	
-	public MessageFactory(String name, String senderAddress) {
-		this.name = name;
-		this.senderAddress = senderAddress;
-	}
-	
-	public Message getMesage(String text) {
-		return new Message(text, name, senderAddress);
-	}
-	
-	public Message getMesage(String text, int destination) {
-		return new Message(text, name, senderAddress, destination);
-	}
+    
+    String name;
+    String senderAddress;
+    
+    public MessageFactory(String name, String senderAddress) {
+        this.name = name;
+        this.senderAddress = senderAddress;
+    }
+    
+    public Message getMesage(String text) {
+        return new Message(text, name, senderAddress);
+    }
+    
+    public Message getMesage(String text, int destination) {
+        return new Message(text, name, senderAddress, destination);
+    }
 }
